de4af1346e722ffee926da55a67928cd98c6b656
JakeKalstad <JakeKalstad@gmail.com>
2014-11-01 02:05:52 -0600

    smtp work, options and a demo file included

---
+++ demo.js
@@ -0,0 +1,13 @@
+var bobLog = require('boblog');
+
+function testError() {
+	bobLog.error("Uh oh, an error occurred..");
+}
+
+function testDebug() {
+	bobLog.debug("Whats going on here...");
+}
+
+console.log('Beginning test');
+testError();
+testDebug();
--- index.js
+++ index.js
@@ -1,20 +1,105 @@
 var fs = require('fs');
 var path = require('path');
-function Logger() {
+var email = require("mailer");
+var stackTrace = require("stack-trace/lib/stack-trace");
+var printf = require('util').format;
+function Logger (options) {
     'use strict';
     this.options = null;
-    function formatMsg(msg) {
+    this.formatMsg = function (msg) {
         var now = new Date();
         return now.toString() + msg + "\n";
-    }
-    this.Log = function (msg) {
-        var locpath = path.dirname(require.main.filename) ;
-        console.log(locpath );
-        fs.appendFile(path.dirname(require.main.filename) + "/log.bob", formatMsg(msg), function (err) {
+    };
+    
+    this.defaultOptions = function () {
+        return {
+            filePath : path.dirname(require.main.filename),
+            fileName : "log.bob",
+            file_on_error : true,
+            file_on_debug : true,
+            
+            smtp : false,
+            smtp_ssl : true,
+            smtp_host : undefined,
+            smtp_port : 465,
+            smtp_domain : "[127.0.0.1]",
+            smtp_auth : 'login',
+            smtp_username : undefined,
+            smtp_password : undefined,
+            smtp_recipients : undefined,
+            smtp_reply_to : undefined,
+            smtp_from : undefined,
+            email_on_error : true,
+            email_on_debug : true,
+            
+            debug : false
+        };
+    };
+    
+    this.formatMsgForEmail = function (msg) {
+        
+    };
+    
+    this.sendEmail = function (msg) {
+        if(!this.options.smtp_recipients) {
+            console.log("No SMTP recipients defined, no emails are being sent...");
+            return;
+        }
+        if(this.options.smtp_host) {
+            console.log("No SMTP host defined, no emails are being sent...");
+            return;
+        }
+        var recipients = this.options.smtp_recipients.split(',');
+        recipients.map(function (recpAddy) {
+            email.send({
+                ssl: this.options.smtp_ssl,
+                host : this.options.smtp_host,
+                port : this.options.smtp_port,
+                domain : this.options.smtp_domain,
+                to : recpAddy,
+                from : this.options.smtp_from,
+                subject : "BobLog - Log",
+                reply_to : this.options.smtp_reply_to,
+                body: this.formatMsgForEmail(msg),
+                authentication : this.options.smtp_auth,
+                username : this.options.smtp_username,
+                password : this.options.smtp_password,
+                debug: this.options.debug
+            },
+            function(err, result){
+                if(err){ console.log(err); }
+            });   
+        });
+    };
+    
+    this.writeToFile = function (msg) {
+        fs.appendFile(this.options.filePath + "/" + this.options.fileName, msg, function (err) {
             if (err) {
                 console.log(err);
             }
         });
+    }
+    
+    this.Log = function (msg, isError) {
+        msg = this.formatMsg(msg);
+        if(!this.options) {
+            this.options = this.defaultOptions();
+        } 
+        if(isError) {
+            if(this.options.file_on_error) {
+                this.writeToFile("Error: " + msg);    
+            }
+            if(this.options.email_on_error) {
+                this.sendEmail(msg);   
+            }
+        } else { 
+            if(this.options.file_on_debug) {
+                this.writeToFile("Debug: " + msg);    
+            }
+            if(this.options.email_on_debug) {
+                this.sendEmail(msg);   
+            } 
+        }
         console.log(msg);
     };
 }
@@ -23,22 +108,35 @@ var Log = (function () {
     'use strict';
     var logger;
     return {
-        Get : function () {
+        Get : function (options) {
             if (!logger) {
                 logger = new Logger();
+            }
+            if(options) {
+                logger.options = options;   
             }
             return logger;
         }
     };
 }());
 
-module.exports = {
-    log : function (msg) {
-        'use strict';
-        Log.Get().Log(msg);
+module.exports = { 
+    error : function(msg) {
+         'use strict';
+        var thistrace = stackTrace.get(); 
+        var parent_name = thistrace[1].getFunctionName(); 
+        var parent_eval = thistrace[1].getEvalOrigin(); 
+        Log.Get().Log(printf("[%s][%s]: %s", parent_eval, parent_name, msg), true);
+    },
+    debug : function(msg) {
+         'use strict';
+        var thistrace = stackTrace.get(); 
+        var parent_name = thistrace[1].getFunctionName(); 
+        var parent_eval = thistrace[1].getEvalOrigin(); 
+        Log.Get().Log(printf("[%s][%s]: %s", parent_eval, parent_name, msg));
     },
     configure : function (options) {
         'use strict';
-        Log.Get().options = options;
+        Log.Get(options);
     }
 };
