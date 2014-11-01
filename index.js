3b53cf4dfd04b91404957666d574e5523f3afd44
JakeKalstad <JakeKalstad@gmail.com>
2014-10-31 19:45:30 -0600

    Logging to console

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
+function Logger() {
+    'use strict';
+    this.options = null;
+    this.Log = function (msg) {
+        console.log(msg);
+    };
+}
+
+var Log = (function () {
+    'use strict';
+    var logger;
+    return {
+        Get : function () {
+            if (!logger) {
+                logger = new Logger();
+            }
+            return logger;
+        }
+    };
+}());
+
 module.exports = {
-    log : function () {
+    log : function (msg) {
         'use strict';
+        Log.Get().Log(msg);
+    },
+    configure : function (options) {
+        'use strict';
+        Log.Get().options = options;
     }
 };
